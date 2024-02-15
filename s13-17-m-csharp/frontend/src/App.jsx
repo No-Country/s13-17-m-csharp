b94efb60d05ee23e7e3cbb95e6ad2d32cd7dd3ec
diegohp <diego.blake11@gmail.com>
2024-02-15 07:40:57 -0600

    [ADD] prettier config and first header version

--- s13-17-m-csharp/frontend/src/App.jsx
+++ s13-17-m-csharp/frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import Test from './components/test'
+import { useState } from 'react';
+import './App.css';
+import Test from './components/test';
+import { Header } from './components/shared/Header/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Test />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
