1964856fc6d1db1c8c064b25e4c1139ebbf77783
kevin joel noviello <kjnoviello@gmail.com>
2024-02-18 17:02:10 -0300

    add volver component

--- s13-17-m-csharp/frontend/src/App.jsx
+++ s13-17-m-csharp/frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import Test from './components/test'
+import Volver from './components/Volver/Volver'
 
 function App() {
   return (
     <>
+      <Volver />
       <Test />
     </>
   )
---
+++ s13-17-m-csharp/frontend/src/components/Volver/Volver.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import './Volver.css'
+
+const Volver = () => {
+    return (
+        <div className='containerVolver'>
+            <a href="">
+                <img src="./Back.svg" alt="back arrow" />
+                <p>Titulo por props</p>
+            </a>
+        </div>
+    )
+}
+
+export default Volver
