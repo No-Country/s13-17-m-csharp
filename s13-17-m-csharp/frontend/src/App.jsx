4ff875994885a7d25533ab13035003935575b0ad
kevin joel noviello <kjnoviello@gmail.com>
2024-02-19 10:49:16 -0300

    change color-card

--- s13-17-m-csharp/frontend/src/App.jsx
+++ s13-17-m-csharp/frontend/src/App.jsx
@@ -1,12 +1,12 @@
-
-import { useState } from 'react';
 import './App.css';
 import Test from './components/test';
 import { Layout } from './components/shared/Layout/Layout';
+import Volver from './components/Volver/Volver';
 
 function App() {
   return (
     <Layout>
+      <Volver />
       <Test />
     </Layout>
   );
