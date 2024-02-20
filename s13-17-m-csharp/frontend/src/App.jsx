c8e438bcbd08e1eb4ad6431289ddfba86c9fb0c8
LauryCGit <93162728+LauryCGit@users.noreply.github.com>
2024-02-20 01:09:01 -0300

    staff

--- s13-17-m-csharp/frontend/src/App.jsx
+++ s13-17-m-csharp/frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+//import { useState } from 'react';
 import './App.css';
 import Test from './components/test';
+import Staff from './components/shared/Staff/Staff';
 import { Header } from './components/shared/Header/Header';
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <>
       <Header />
       <Test />
+      <p className='p-16 text-center h-30 bg-teal-100'>Aqui va el Carrusel</p>
+      <Staff/>
     </>
   );
 }
---
+++ s13-17-m-csharp/frontend/src/components/Staff/Staff.jsx
@@ -0,0 +1,20 @@
+
+const Staff = () => {
+    return (
+        <div>
+            <div className="p-5">
+                <h4 className="text-center ">Staff</h4>
+            </div>
+
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+        
+
+        </div>
+        
+    )
+  }
+  
+  export default Staff;
