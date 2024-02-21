52a868b9fb845435d2d058a9e69eee0026025309
Alejo <alejohurtadotesta@gmail.com>
2024-02-20 22:29:37 -0300

    Se crea pagina de inicio, login y registros con sus respectivos componentes y diseños

--- s13-17-m-csharp/frontend/src/App.jsx
+++ s13-17-m-csharp/frontend/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react';
 import './App.css';
 import Test from './components/test';
 import { Header } from './components/shared/Header/Header';
+import Login from './pages/Login/Login';
+import Inicio from './pages/Inicio/Inicio';
+import Register1 from './pages/Registros/Register1';
+import Register2 from './pages/Registros/Register2';
 
 function App() {
   return (
     <>
-      <Header />
-      <Test />
+      <Login />
+      <Inicio />
+      <Register1 />
+      <Register2 />
     </>
   );
 }
---
+++ s13-17-m-csharp/frontend/src/components/shared/Buttons/ButtonsRegistrarse/ButtonRegisterFill.jsx
@@ -0,0 +1,9 @@
+import './buttonRegisterFill.css';
+
+export default function ButtonRegisterFill() {
+  return (
+    <div>
+      <button className="btn-registerfill">Registrarse</button>
+    </div>
+  );
+}
