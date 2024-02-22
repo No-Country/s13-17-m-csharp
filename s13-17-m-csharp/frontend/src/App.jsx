62d0c9b8e44e150960740d888482c525584971d6
diegohp <diego.blake11@gmail.com>
2024-02-22 00:46:02 -0600

    [ADD] first version of Doctor Card

--- s13-17-m-csharp/frontend/src/App.jsx
+++ s13-17-m-csharp/frontend/src/App.jsx
@@ -1,22 +1,24 @@
 import './App.css';
-import Slider from './components/Slider/Slider'
+import Slider from './components/Slider/Slider';
 import Staff from './components/Staff/Staff';
-import Search from './components/search/Search'
+import Search from './components/search/Search';
 import Login from './pages/Login/Login';
 import Inicio from './pages/Inicio/Inicio';
 import Register1 from './pages/Registros/Register1';
 import Register2 from './pages/Registros/Register2';
 import { Layout } from './components/shared/Layout/Layout';
+import { DoctorCard } from './components/DoctorCard/DoctorCard';
 import Volver from './components/Volver/Volver';
 
 function App() {
   return (
     <>
       <Layout>
-        <Slider/>
-        <Staff/>
-        <Search/>
+        <Slider />
+        <Staff />
+        <Search />
         <Volver />
+        <DoctorCard />
         <Login />
         <Inicio />
         <Register1 />
