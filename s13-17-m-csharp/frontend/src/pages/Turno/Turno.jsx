9198c3e37c0d046825c3135af097728b085a4a00
kevin joel noviello <kjnoviello@gmail.com>
2024-02-24 14:34:53 -0300

    first commit

---
+++ s13-17-m-csharp/frontend/src/pages/Turno/Turno.jsx
@@ -0,0 +1,13 @@
+import { DoctorCard } from '../../components/DoctorCard/DoctorCard'
+import style from './Turno.module.css'
+
+const Turno = () => {
+    return (
+        <main className={style.mainContainer}>
+            <DoctorCard />
+        </main>
+    )
+}
+
+
+export default Turno
