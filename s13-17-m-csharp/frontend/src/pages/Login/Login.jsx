d360d7eadea80a2a24bd36eb2c823f932cd1bbb4
kevin joel noviello <kjnoviello@gmail.com>
2024-02-26 15:56:11 -0300

    fix menu hamburger merge

--- s13-17-m-csharp/frontend/src/pages/Login/Login.jsx
+++ s13-17-m-csharp/frontend/src/pages/Login/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { NavLink } from 'react-router-dom';
 
 import ButtonLogin from '../../components/shared/Buttons/ButtonIniciarSesion/ButtonLogin';
@@ -11,37 +12,7 @@ export default function Login({isLoggedIn,setIsLoggedIn}) {
 
 
   return (
-    
-    <div>
-      <h1 className="title-iniciarsesion">Iniciar sesión</h1>
-      <form className="form-iniciarsesion">
-        <input
-          type="email"
-          name="email"
-          placeholder="Mail"
-          required
-          className="input-iniciarsesion"
-        />
-        <input
-          type="password"
-          name="contraseña"
-          placeholder="Contraseña"
-          className="input-iniciarsesion"
-          required
-        />
-      </form>
-      <a href="#" className="text-iniciarsesion">
-        Olvide mi contraseña
-      </a>
-      <div className="btns-iniciarsesion">
-        <NavLink to='/logueado'>
-          <ButtonLogin isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-        </NavLink>
-        <NavLink to='register/step1'>
-          <ButtonRegister />
-        </NavLink>
-
-          {/*<div className="container-iniciarmain">
+        <div className="container-iniciarmain">
       <div className="container-iniciarsesion">
         <h1 className="title-iniciarsesion">Iniciar sesión</h1>
         <form className="form-iniciarsesion">
@@ -70,14 +41,13 @@ export default function Login({isLoggedIn,setIsLoggedIn}) {
           Olvide mi contraseña
         </a>
         <div className="btns-iniciarsesion">
-          <NavLink to='/'>
-            <ButtonLogin />
+          <NavLink to='/logueado'>
+            <ButtonLogin isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
           </NavLink>
           <div className="ocultarbtn-iniciarsesion">
             <ButtonRegister />
           </div>
-        </div>*/}
-
+        </div>
       </div>
     </div>
   );
