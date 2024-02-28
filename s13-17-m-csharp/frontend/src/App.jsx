a54f4a02c968e1a9cb2a8fc1dd936bfd557815c7
Alejo <alejohurtadotesta@gmail.com>
2024-02-27 23:37:13 -0300

    Se crea la vista de la pagina de "Nosotros" y se configura la ruta en el app.jsx y header.jsx

--- s13-17-m-csharp/frontend/src/App.jsx
+++ s13-17-m-csharp/frontend/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Route, Routes,Navigate } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate
+} from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login/Login';
 import Inicio from './pages/Inicio/Inicio';
@@ -10,45 +15,91 @@ import Volver from './components/Volver/Volver';
 import Home from './pages/Home/Home';
 import { useState } from 'react';
 import Turno from './pages/Turno/Turno';
+import Nosotros from './pages/Nosotros/Nosotros';
 
 function App() {
-
-  // state para saber si está registrado o no  
+  // state para saber si está registrado o no
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/inicio' element={<Layout>
-                                            <Volver title={"Volver"} />
-                                            <Inicio />
-                                          </Layout>}/>
-        <Route path='/login' element={<Layout>
-                                        <Volver title={"Volver"} />
-                                        <Login isLoggedIn={isLoggedIn}  setIsLoggedIn= {setIsLoggedIn} />
-                                      </Layout>}/>
-        <Route path='/register/step1' element={<Layout>
-                                        <Volver title={"Volver"} />
-                                        <Register1 />
-                                      </Layout>}/>
-        <Route path='/register/step2' element={<Layout>
-                                        <Volver title={"Volver"} /> 
-                                        <Register2 />
-                                      </Layout>}/>
-        <Route path='/docProfile' element={<Layout>
-                                        <Volver title={"Volver"} />
-                                        <DoctorCard />
-                                      </Layout>}/>
-        <Route path='/agenda' element={<Layout>
-                                        <Volver title={"Confirmar fecha"} />
-                                        <Turno />
-                                      </Layout>}/>
-        <Route path="/logueado" element={ isLoggedIn ? (<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
-              </Home>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/inicio"
+          element={
+            <Layout>
+              <Volver title={'Volver'} />
+              <Inicio />
+            </Layout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Volver title={'Volver'} />
+              <Login
+                isLoggedIn={isLoggedIn}
+                setIsLoggedIn={setIsLoggedIn}
+              />
+            </Layout>
+          }
+        />
+        <Route
+          path="/register/step1"
+          element={
+            <Layout>
+              <Volver title={'Volver'} />
+              <Register1 />
+            </Layout>
+          }
+        />
+        <Route
+          path="/register/step2"
+          element={
+            <Layout>
+              <Volver title={'Volver'} />
+              <Register2 />
+            </Layout>
+          }
+        />
+        <Route
+          path="/docProfile"
+          element={
+            <Layout>
+              <Volver title={'Volver'} />
+              <DoctorCard />
+            </Layout>
+          }
+        />
+        <Route
+          path="/nosotros"
+          element={
+            <Layout>
+              <Volver title={'Volver'} />
+              <Nosotros />
+            </Layout>
+          }
+        />
+        <Route
+          path="/agenda"
+          element={
+            <Layout>
+              <Volver title={'Confirmar fecha'} />
+              <Turno />
+            </Layout>
+          }
+        />
+        <Route
+          path="/logueado"
+          element={
+            isLoggedIn ? (
+              <Home
+                isLoggedIn={isLoggedIn}
+                setIsLoggedIn={setIsLoggedIn}></Home>
             ) : (
-              <Navigate to="/" /> 
+              <Navigate to="/" />
             )
           }
         />
