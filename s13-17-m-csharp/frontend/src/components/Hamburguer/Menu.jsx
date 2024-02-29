16ad6f933132ce8d6c1c4f9cd5299c5aa6fed62d
kevin joel noviello <kjnoviello@gmail.com>
2024-02-29 02:54:39 -0300

    final

--- s13-17-m-csharp/frontend/src/components/Hamburguer/Menu.jsx
+++ s13-17-m-csharp/frontend/src/components/Hamburguer/Menu.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-import styles from '../Hamburguer/menu.module.css';
-import { Turn as Hamburger } from 'hamburger-react';
-import user from '../../assets/img/User.svg';
 import { useState } from 'react';
+import { Turn as Hamburger } from 'hamburger-react';
+import styles from '../Hamburguer/menu.module.css';
+import user from '../../assets/img/User.svg';
 
 import ContentHamburguer from '../shared/ContentHamburguer/ContentHamburguer';
 
@@ -15,19 +15,7 @@ const Menu = ({ isLoggedIn, setIsLoggedIn }) => {
 
   return (
     <div>
-      <header className={`${styles.header} w-full  h-14 flex  justify-between items-center font-font-main text-base pt-5 mb-5`} >
-        {/*  <source media="(min-width:768px)" srcSet={desktopLogo} />*/}
-
-        <div>
-          <img
-            className="ml-4 w-28 md:w-36 md:ml-10"
-            alt="logo"
-            src={mobileLogo}
-          />
-        </div>
-
-        <div className="flex items-center gap-3 md:order-1 ">
-
+        <div className="flex items-center gap-3 md:order-1 mr-[16px] md:mr-[40px]">
           <h4 className={`${styles.font} text-[#333333]`}>
             ¡Hola Julia!{' '}
           </h4>
@@ -56,3 +44,84 @@ const Menu = ({ isLoggedIn, setIsLoggedIn }) => {
 };
 
 export default Menu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /* eslint-disable react/prop-types */
+// import styles from '../Hamburguer/menu.module.css';
+// import { Turn as Hamburger } from 'hamburger-react';
+// import user from '../../assets/img/User.svg';
+// import { useState } from 'react';
+// import mobileLogo from '../../assets/img/mobileLogo.svg'
+
+// import ContentHamburguer from '../shared/ContentHamburguer/ContentHamburguer';
+
+// const Menu = ({ isLoggedIn, setIsLoggedIn }) => {
+//   const [open, setOpen] = useState(false);
+
+//   const handleToggle = () => {
+//     setOpen(!open);
+//   };
+
+//   return (
+//     <div>
+//       <header className={`${styles.header} w-full  h-14 flex  justify-between items-center font-font-main text-base pt-5 mb-5`} >
+//         {/*  <source media="(min-width:768px)" srcSet={desktopLogo} />*/}
+
+//         <div>
+//           <img
+//             className="ml-4 w-28 md:w-36 md:ml-10"
+//             alt="logo"
+//             src={mobileLogo}
+//           />
+//         </div>
+
+//         <div className="flex items-center gap-3 md:order-1 ">
+
+//           <h4 className={`${styles.font} text-[#333333]`}>
+//             ¡Hola Julia!{' '}
+//           </h4>
+//           <div className="md:hidden">
+//             <Hamburger
+//               onToggle={handleToggle}
+//               size={30}
+//               color="#097DCA"
+//             />
+//           </div>
+//           <div className="hidden md:block cursor-pointer ">
+//             <img onClick={handleToggle} className={`${styles.imgUser}`} src={user} alt="" />
+//           </div>
+//         </div>
+
+//       {open ? (
+//         <>
+//           <ContentHamburguer
+//             isLoggedIn={isLoggedIn}
+//             setIsLoggedIn={setIsLoggedIn}
+//           />
+//         </>
+//       ) : null}
+//     </header>
+//     </div>
+//   );
+// };
+
+// export default Menu;
--- s13-17-m-csharp/frontend/src/components/shared/Layout/Layout.jsx
+++ s13-17-m-csharp/frontend/src/components/shared/Layout/Layout.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/prop-types */
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
+
 const Layout = ({ children,isLoggedIn,setIsLoggedIn }) => {
-  console.log(isLoggedIn);
 
   return (
     <>
       {isLoggedIn ? <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />   :  <Header /> }
-      {/* <Header  isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} /> */}
         {children}
       <Footer />
     </>
